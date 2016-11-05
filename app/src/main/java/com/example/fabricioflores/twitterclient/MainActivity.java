8ed8e2f06175d62cf0bd4a1c4e1d8435d6378af9
Fabricio Flores <fabrifloresg@gmail.com>
2016-11-05 14:02:24 +0100

    Add timeline and search timeline

--- app/src/main/java/com/example/fabricioflores/twitterclient/MainActivity.java
+++ app/src/main/java/com/example/fabricioflores/twitterclient/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.fabricioflores.twitterclient;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.tweetui.SearchTimeline;
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "Q8Dt86zMMLTqwrOdzGxCuWGcl";
@@ -19,5 +23,12 @@ public class MainActivity extends Activity {
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
+        SearchTimeline searchTimeline = new SearchTimeline.Builder()
+                .query("#loja")
+                .build();
+        TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+                .setTimeline(searchTimeline)
+                .build();
+        setListAdapter(adapter);
     }
 }
